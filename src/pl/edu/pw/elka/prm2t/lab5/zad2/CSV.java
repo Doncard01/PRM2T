692fb8ced21781148d38a9ff9439da986b1b55fb
Adam Staciwa <01169353@pw.edu.pl>
2022-04-28 12:03:47 +0200

    zad 2 całe

--- src/pl/edu/pw/elka/prm2t/lab5/zad2/CSV.java
+++ src/pl/edu/pw/elka/prm2t/lab5/zad2/CSV.java
@@ -1,19 +1,79 @@
 package pl.edu.pw.elka.prm2t.lab5.zad2;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class CSV {
-/**
-    List<String[]> csv = new ArrayList<>();
-    try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
-        System.out.println(sa);
+    private List<String[]> csv = new ArrayList<>();
+    private String input;
+    private String output;
+    private String sep1;
+    private String sep2;
+
+    public CSV(String input, String output, String sep1, String sep2) {
+        this.input = input;
+        this.output = output;
+        this.sep1 = sep1;
+        this.sep2 = sep2;
     }
 
-    public CSV() throws FileNotFoundException {
+    public void read() {
+        try (BufferedReader br = new BufferedReader(new FileReader(input))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                String[] values = line.split(sep1);
+                csv.add(values);
+            }
+    } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
- **/
+
+    public void write() {
+        try (FileWriter writer = new FileWriter(output);) {
+            for (String[] line : csv) {
+                for (String word : line) {
+                    writer.append(word).append(sep2);
+                }
+                writer.append("\n");
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String getValue(int row, int column) {
+        return csv.get(row)[column];
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CSV csv1 = (CSV) o;
+        return Objects.equals(csv, csv1.csv) && Objects.equals(input, csv1.input) && Objects.equals(output, csv1.output) && Objects.equals(sep1, csv1.sep1) && Objects.equals(sep2, csv1.sep2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(csv, input, output, sep1, sep2);
+    }
+
+    @Override
+    public String toString() {
+        return "Obiekt klasy CSV, kopiujący zawartość z " + input + ", do"
+        + output + " i zmieniająca " + sep1 + " na " + sep2;
+    }
+
+    public static void main(String[] args) {
+        CSV test = new CSV("resource/ufo.csv", "resource/zmiana.csv", ",", "/");
+
+        test.read();
+        test.write();
+        System.out.println(test.getValue(3, 1));;
+
+    }
 }
--- test/pl/edu/pw/elka/prm2t/cw5/copyFileTest/attempt1/CopyFileTest.java
+++ test/pl/edu/pw/elka/prm2t/cw5/copyFileTest/attempt1/CopyFileTest.java
@@ -1,7 +1,14 @@
 package pl.edu.pw.elka.prm2t.cw5.copyFileTest.attempt1;
 
-import org.junit.jupiter.api.Test;
+/**
+ * import pl.edu.pw.elka.prm2t.cw5.Zadanie102_CopyFile; ??
+ */
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.testng.annotations.Test;
+
 import pl.edu.pw.elka.prm2t.cw5.Zadanie102_CopyFile;
 
 public class CopyFileTest {
